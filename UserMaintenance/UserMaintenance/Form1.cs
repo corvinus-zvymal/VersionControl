602945c7eacab0c6799b043bed76dcda86583968
Márton Pethő <marton.petho@stud.uni-corvinus.hu>
2021-10-01 15:03:07 +0200

    próba

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.FullName;
+            button1.Text = Resource1.Add;
+            button2.Text = Resource1.FileWrite;
+            button3.Text = Resource1.Delete;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = textBox1.Text
+            };
+            users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var delete = listBox1.SelectedItem;
+            if (delete != null)
+                users.Remove((User)delete);
         }
     }
 }
