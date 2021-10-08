9398d624874c2bb00cabc91fe37e95cc4faabaa8
Márton Pethő <marton.petho@stud.uni-corvinus.hu>
2021-10-08 17:15:06 +0200

    CreateExcel

--- UserMaintenance/WindowsFormsApp1/Form1.cs
+++ UserMaintenance/WindowsFormsApp1/Form1.cs
@@ -16,14 +16,53 @@ namespace WindowsFormsApp1
     {
         List<Flat> lakasok;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
         public void LoadData()
         {
             lakasok = context.Flats.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                
+                xlApp = new Excel.Application();
+
+                
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+               
+                xlSheet = xlWB.ActiveSheet;
+
+               
+               // CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+               
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
         }
     }
 }
