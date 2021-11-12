bb3e33dee340d8c166868bd45c1b15cdc3ec1c96
Márton Pethő <marton.petho@stud.uni-corvinus.hu>
2021-11-12 10:30:13 +0100

    Task2

--- HetedikGyakorlat/HetedikGyakorlat/Form1.cs
+++ HetedikGyakorlat/HetedikGyakorlat/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HetedikGyakorlat.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,50 @@ namespace HetedikGyakorlat
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _ballFactory;
+
+        public BallFactory BallFactory
+        {
+            get { return _ballFactory; }
+            set { _ballFactory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            BallFactory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = BallFactory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = -ball.Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var lastPosition = 0;
+
+            
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if (item.Left > lastPosition)
+                {
+                    lastPosition = item.Left;
+                }
+            }
+
+            if (lastPosition>=1000)
+            {
+                var oldestBall = _balls[0];
+                _balls.Remove(oldestBall);
+                mainPanel.Controls.Remove(oldestBall);
+            }
         }
     }
 }
