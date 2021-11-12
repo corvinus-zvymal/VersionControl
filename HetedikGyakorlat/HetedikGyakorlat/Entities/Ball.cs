3d677b91a3dc1f1dab1942ba5deb0429ea6e886e
Márton Pethő <marton.petho@stud.uni-corvinus.hu>
2021-11-12 10:09:50 +0100

    Task1

---
+++ HetedikGyakorlat/HetedikGyakorlat/Entities/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HetedikGyakorlat.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics g)
+        {
+            Brush ecset = new SolidBrush(Color.Blue);
+            g.FillEllipse(
+                ecset,
+                0,
+                0,
+                Width,
+                Height);
+        }
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+
+    }
+}
