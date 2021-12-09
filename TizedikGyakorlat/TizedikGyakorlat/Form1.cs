2f9a3d71edcd71b57a7dce958111e16ac661c928
Márton Pethő <marton.petho@stud.uni-corvinus.hu>
2021-12-09 17:57:56 +0100

    Task3

--- TizedikGyakorlat/TizedikGyakorlat/Form1.cs
+++ TizedikGyakorlat/TizedikGyakorlat/Form1.cs
@@ -17,14 +17,22 @@ namespace TizedikGyakorlat
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
 
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
-            gc.AddPlayer();
-            gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
             this.Controls.Add(ga);
         }
     }
